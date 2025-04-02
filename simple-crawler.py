817f1c3016d375904da0838096bc378a29e7c729
Nick Baynham <nickbaynham@gmail.com>
2025-04-02 14:27:34 -0700

    using BS4 in scripts

--- simple-crawler.py
+++ simple-crawler.py
@@ -1,9 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
 target_url = "https://www.scrapingcourse.com/ecommerce/"
 print('Target URL is', target_url)
 
 response = requests.get(target_url)
+response.raise_for_status()
 
 # Print the HTTP status code
 print("Status Code:", response.status_code)
+
+# Let's see the HTML
+print(response.text)
+
+# parse the HTML
+soup = BeautifulSoup(response.text, "html.parser")
